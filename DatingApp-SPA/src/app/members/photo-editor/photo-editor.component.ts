00f30eceee564e931782770c9e8ed13afe3e7b48
Richard <richard.scott@nottscc.gov.uk>
2018-10-02 03:20:10 +0100

    End of Section 12

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -52,6 +52,12 @@ export class PhotoEditorComponent implements OnInit {
           isMain: res.isMain
         };
         this.photos.push(photo);
+        if (photo.isMain) {
+          this.authService.changeMemberPhoto(photo.url);
+         // this.getMemberPhotoChange.emit(photo.url);
+          this.authService.currentUser.photoUrl = photo.url;
+          localStorage.setItem('user', JSON.stringify(this.authService.currentUser));
+        }
     }
    };
  }
@@ -61,7 +67,7 @@ export class PhotoEditorComponent implements OnInit {
       this.currentMain.isMain = false;
       photo.isMain = true;
       this.authService.changeMemberPhoto(photo.url);
-      this.getMemberPhotoChange.emit(photo.url);
+     // this.getMemberPhotoChange.emit(photo.url);
       this.authService.currentUser.photoUrl = photo.url;
       localStorage.setItem('user', JSON.stringify(this.authService.currentUser));
 
--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -32,6 +32,7 @@ namespace DatingApp.API.Helpers
             CreateMap<UserForUpdatesDto, User>();
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto,Photo>();
+            CreateMap<UserForRegisterDto, User>();
         }
     }
 }
